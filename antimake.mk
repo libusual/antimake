67c6c878157e50e780e4869b06440584abc8bed0
Marko Kreen <markokr@gmail.com>
2012-03-20 22:01:17 +0200

    Remove libusual hacks

--- antimake.mk
+++ antimake.mk
@@ -666,31 +666,6 @@ ForEachTarget2 = $(foreach tgt,$($(1)),$(call $(5),$(call CleanName,$(tgt)),$(tg
 ForEachTarget = $(call ForEachList,ForEachTarget2,$(2),$(1))
 
 
-##
-## Utility functions for libusual link
-##
-
-_USUAL_DIR = $(call JoinPath,$(srcdir),$(USUAL_DIR))
-
-# module names from sources (plus headers)
-UsualMods = $(trace1)$(shell $(_USUAL_DIR)/find_modules.sh $(_USUAL_DIR) $(wildcard $(addprefix $(srcdir)/,$(1))))
-
-# full-path sources based on module list
-UsualSrcsFull = $(trace1)$(wildcard $(addprefix $(_USUAL_DIR)/usual/,$(addsuffix *.[ch],$(1))))
-
-# remove USUAL_DIR
-UsualStrip = $(trace1)$(subst $(_USUAL_DIR)/,,$(1))
-
-# simple-path sources based on module list
-UsualSrcs = $(call UsualStrip,$(call UsualSrcsFull,$(1)))
-
-# objs with objdir from source file list (1-cleantgt, 2-src list)
-UsualObjs = $(call SourceObjs,$(1),$(call UsualSrcs,$(call UsualMods,$(2))))
-
-# usual sources from user source file list
-UsualSources = $(call UsualSrcsFull,$(call UsualMods,$(1)))
-
-
 ## EMBED_SUBDIRS relocations
 
 ## add subdir to files
@@ -927,19 +902,6 @@ $(1)_LIBS += $$(filter -%,$$($(1)_LDADD))
 $(ELSE)
 $(1)_OBJS += $$(filter-out -%,$$($(1)_LIBADD))
 $(1)_LIBS += $$(filter -%,$$($(1)_LIBADD))
-$(ENDIF)
-
-# embed libusual objects directly
-$(IFEQ) ($$($(1)_EMBED_LIBUSUAL),1)
-$(1)_USUAL_SRCS = $$(call UsualSources,$$($(1)_ALLSRCS))
-$(1)_OBJS += $$(call UsualObjs,$(1),$$($(1)_SOURCES) $$(nodist_$(1)_SOURCES))
-$(1)_CPPFLAGS += -I$$(USUAL_DIR)
-$(IFEQ) ($$(filter $$(USUAL_DIR),$(VPATH)),)
-VPATH += $$(USUAL_DIR)
-$(ENDIF)
-$(IFNEQ) ($$(srcdir),$$(builddir),)
-VPATH += $$(call JoinPath,$$(srcdir),$$(USUAL_DIR))
-$(ENDIF)
 $(ENDIF)
 
 # autodetect linker, unless given
